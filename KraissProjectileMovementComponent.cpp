eb23f5187ba84fbb2334b5d3fab80ad7285c20d0
Karla <karkraiss@gmail.com>
2021-09-21 15:12:07 -0700

    Basic movement (not checking what we hit yet)

--- KraissProjectileMovementComponent.cpp
+++ KraissProjectileMovementComponent.cpp
@@ -1,1 +1,77 @@
 ﻿#include "KraissProjectileMovementComponent.h"
+
+
+UKraissProjectileMovementComponent::UKraissProjectileMovementComponent()
+{
+	// assuming the spawner passed the projectile the proper direction when launching the projectile
+}
+
+void UKraissProjectileMovementComponent::TickComponent(float DeltaTime, ELevelTick Tick, FActorComponentTickFunction* ThisTickFunction)
+{
+	UMovementComponent::TickComponent(DeltaTime, Tick, ThisTickFunction);
+
+	FHitResult Hit;
+	// not simulating physics since assuming we're not networked
+	// move our projectile
+
+	const FVector MoveDelta = ComputeMoveDelta(InitialVelocity, DeltaTime);
+	
+	MoveUpdatedComponent(MoveDelta, MoveDelta.Rotation(), true, &Hit);
+
+	// Handle hit result after movement
+	if (Hit.bBlockingHit)
+	{
+		
+	}
+}
+
+// modified from UProjectileMovementComponent.cpp
+FVector UKraissProjectileMovementComponent::ComputeMoveDelta(const FVector& InVelocity, float DeltaTime) const
+{
+	// Velocity Verlet integration (http://en.wikipedia.org/wiki/Verlet_integration#Velocity_Verlet)
+	// The addition of p0 is done outside this method, we are just computing the delta.
+	// p = p0 + v0*t + 1/2*a*t^2
+
+	// We use ComputeVelocity() here to infer the acceleration, to make it easier to apply custom velocities.
+	// p = p0 + v0*t + 1/2*((v1-v0)/t)*t^2
+	// p = p0 + v0*t + 1/2*((v1-v0))*t
+
+	const FVector NewVelocity = ComputeVelocity(InVelocity, DeltaTime);
+	const FVector Delta = (InVelocity * DeltaTime) + (NewVelocity - InVelocity) * (0.5f * DeltaTime);
+	return Delta;
+}
+
+// modified from UProjectileMovementComponent.cpp
+FVector UKraissProjectileMovementComponent::ComputeVelocity(FVector InitialVelocity, float DeltaTime) const
+{
+	// v = v0 + a*t
+	const FVector Acceleration = ComputeAcceleration(InitialVelocity, DeltaTime);
+	const FVector NewVelocity = InitialVelocity + (Acceleration * DeltaTime);
+
+	return LimitVelocity(NewVelocity);
+}
+
+// modified from UProjectileMovementComponent.cpp
+FVector UKraissProjectileMovementComponent::ComputeAcceleration(const FVector& InVelocity, float DeltaTime) const
+{
+	FVector Acceleration(FVector::ZeroVector);
+
+	Acceleration.Z += GetGravityZ();
+
+	// TODO: calculate the pending force to add (or 'subtract') for option of gas fueled projectiles
+	// Acceleration += PendingForce;
+	
+	return Acceleration;
+}
+
+// modified from UProjectileMovementComponent.cpp
+FVector UKraissProjectileMovementComponent::LimitVelocity(FVector NewVelocity) const
+{
+	const float CurrentMaxSpeed = ProjectileDefinition.MaxSpeed;
+	if (CurrentMaxSpeed > 0.f)
+	{
+		NewVelocity = NewVelocity.GetClampedToMaxSize(CurrentMaxSpeed);
+	}
+
+	return ConstrainDirectionToPlane(NewVelocity);
+}
--- KraissProjectileMovementComponent.h
+++ KraissProjectileMovementComponent.h
@@ -1,13 +1,27 @@
 ﻿#pragma once=
 #include "GameFramework/MovementComponent.h"
 
+//data
 USTRUCT()
 struct FBaseProjectileDefinition
 {
 	GENERATED_BODY()
 public:
+	// how long this projectile should stay around, a negative number means it should stay indefinitely
 	UPROPERTY(EditAnywhere)
 	float Lifetime;
+	
+	UPROPERTY(EditAnywhere)
+	float InitialSpeed;
+
+	UPROPERTY(EditAnywhere)
+	float MaxSpeed;
+
+	UPROPERTY(EditAnywhere)
+	float MinSpeed;
+
+	UPROPERTY(EditAnywhere)
+	float MaxDistance;
 
 	UPROPERTY(EditAnywhere)
 	float Damage;
@@ -42,18 +56,37 @@ public:
 
 	/**
 	* Total number of bounces before triggering End of Life effect
+	* -1 means no maxbounces, use lifetime to determine End of Life
 	*/
 	UPROPERTY(EditAnywhere)
-	int32 MaxBounces;
+	int8 MaxBounces;
 };
-
-class KraissProjectileMovementComponent : public UMovementComponent
+// work
+class UKraissProjectileMovementComponent : public UMovementComponent
 {
 public:
+	// !! assuming the component is properly initialized & that the spawner successfully set up struct information !!
+	UKraissProjectileMovementComponent();
+
+	void SetInitialVelocity(const FVector Velocity) { InitialVelocity = Velocity; }
+	
+	//Begin UActorComponent Interface
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+	//End UActorComponent Interface
+	
 	// if networked - would need probably want a delegate for the hit result and might want to separate out the ProjectileDefinition so not sending that whole thing over the network
 
 protected:
-	
 	UPROPERTY()
 	FBaseProjectileDefinition ProjectileDefinition;
+	
+	// projectile Launcher should pass this data
+	FVector InitialVelocity;
+
+	// Begin modified from UProjectileMovementComponent.cpp 
+	FVector UKraissProjectileMovementComponent::ComputeMoveDelta(const FVector& InVelocity, float DeltaTime) const;
+	FVector UKraissProjectileMovementComponent::ComputeVelocity(FVector InitialVelocity, float DeltaTime) const;
+	FVector UKraissProjectileMovementComponent::ComputeAcceleration(const FVector& InVelocity, float DeltaTime) const;
+	FVector UKraissProjectileMovementComponent::LimitVelocity(FVector NewVelocity) const;
+	// End modified from UProjectileMovementComponent.cpp
 };
